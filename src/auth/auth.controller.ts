f09b07c91718b656656e916973b66d9c6dec713b
Sylvain Pelissier <sylvain@onenetbeyond.org>
2023-12-21 14:14:48 +0100

    init auth:register() & auth:connect()

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Get, Post} from '@nestjs/common'
+import {AuthService} from './auth.service'
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('register')
+  register() {}
+
+  @Post('connect')
+  connect() {}
+}
