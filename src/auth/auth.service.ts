4de51a6cc74dd39222bc69d6b4934f8bcfd17809
Sylvain Pelissier <sylvain@onenetbeyond.org>
2023-12-21 16:40:45 +0100

    prisma: user model

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,15 +4,15 @@ import {AuthDto} from './dto'
 @Injectable()
 export class AuthService {
   register(dto: AuthDto) {
-    return {
-      status: 201,
-      name: this.register.name,
-    }
-
     // 1. get email & password
     // 2. throw if email is in use
     // 3. hash the password
     // 4. save the user data
+    return {
+      status: 201,
+      name: this.register.name,
+      dto,
+    }
   }
 
   connect() {
