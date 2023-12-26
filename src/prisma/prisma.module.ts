d091ce09fac965471a7fecda03cb44e2318b085a
Sylvain Pelissier <sylvain@onenetbeyond.org>
2023-12-26 13:39:57 +0100

    prisma Svc

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,4 +1,8 @@
 import {Module} from '@nestjs/common'
+import {PrismaService} from './prisma.service'
 
-@Module({})
+@Module({
+  providers: [PrismaService], // accessible inside this module
+  exports: [PrismaService], // accessible outside
+})
 export class PrismaModule {}
