5b08871119c944dbca2856ad883932c3ca531101
Sylvain Pelissier <sylvain@onenetbeyond.org>
2023-12-26 13:54:33 +0100

    inject prisma into auth (2)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,7 +5,9 @@ import {PrismaService} from '../prisma/prisma.service'
 @Injectable()
 export class AuthService {
   constructor(private prismaService: PrismaService) {}
-  registerUser(dto: AuthDto) {
+  async registerUser(dto: AuthDto) {
+    const userExists = await this.prismaService.user.findFirst({where: {email: dto.email}})
+    console.log({userExists})
     // 1. get email & password
     // 2. throw if email is in use
     // 3. hash the password
