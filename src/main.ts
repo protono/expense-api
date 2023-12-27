789d3cb7b2b019208f5fb3e2b44159585e50d7d9
Sylvain Pelissier <sylvain@onenetbeyond.org>
2023-12-27 17:40:35 +0100

    install session

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import {NestFactory} from '@nestjs/core'
 import {AppModule} from './app.module'
+import * as session from 'express-session'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.use(
+    session({
+      secret: 'a3f99b00-8c95-4556-b8a2-fa5b7c086411',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  )
   await app.listen(7357)
 }
 bootstrap()
