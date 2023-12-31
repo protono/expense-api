fbccb85d0523e1d578b403f52aa8e5e22988fa8d
Sylvain Pelissier <sylvain@onenetbeyond.org>
2023-12-31 05:53:43 +0100

    Expense CRUD: get methods

--- src/expense/expense.service.ts
+++ src/expense/expense.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import {ForbiddenException, Injectable, NotFoundException, UnauthorizedException} from '@nestjs/common'
+import {PrismaService} from '../prisma/prisma.service'
 
 @Injectable()
-export class ExpenseService {}
+export class ExpenseService {
+  constructor(private prismaService: PrismaService) {}
+
+  getExpenses(userId: number) {
+    return this.prismaService.expense.findMany({
+      where: {
+        userId,
+      },
+    })
+  }
+
+  async getExpense(userId: number, expenseId: number) {
+    const expense = await this.prismaService.expense.findFirst({where: {id: expenseId}})
+    if (!expense) throw new NotFoundException(`Expense ${expenseId} does not exist`)
+    if (expense.userId !== userId) throw new ForbiddenException(`Access to expense ${expenseId} is forbidden`)
+    return expense
+  }
+
+  async createExpense(userId: number, dto: any) {}
+
+  async updateExpense(userId: number, expenseId: number, dto: any) {}
+
+  async deleteExpense(userId: number, expenseId: number) {}
+}
