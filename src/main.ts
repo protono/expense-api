5832da2daa920fed0f03e167b1adc4646d5bafcc
Sylvain Pelissier <sylvain@onenetbeyond.org>
2024-01-03 17:53:56 +0100

    redis setup

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,30 @@
-import {NestFactory} from '@nestjs/core'
 import {AppModule} from './app.module'
-import * as session from 'express-session'
+import {NestFactory} from '@nestjs/core'
 import {ValidationPipe} from '@nestjs/common'
+import * as session from 'express-session'
+import RedisStore from 'connect-redis'
+import {createClient} from 'redis'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const redisClient = createClient({
+    url: 'redis://localhost:6379',
+  })
+  redisClient.on('error', (err) => {
+    console.log(`Error ${err} while connecting to Redis`)
+  })
+  redisClient.on('connect', () => {
+    console.log(`Successfully connected to Redis`)
+  })
+  redisClient.connect()
   app.use(
     session({
       secret: 'a3f99b00-8c95-4556-b8a2-fa5b7c086411',
       resave: false,
       saveUninitialized: false,
+      store: new RedisStore({
+        client: redisClient,
+      }),
     }),
   )
   app.useGlobalPipes(
@@ -20,4 +35,5 @@ async function bootstrap() {
   )
   await app.listen(7357)
 }
+
 bootstrap()
