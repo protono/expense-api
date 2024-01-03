fde8ac55223c0e2b832685c6040e5c989ab74a57
Sylvain Pelissier <sylvain@onenetbeyond.org>
2024-01-03 11:30:50 +0100

    implement both GETs into controller

--- src/expense/expense.controller.ts
+++ src/expense/expense.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Delete, Get, Param, Patch, Post} from '@nestjs/common'
+import {ExpenseService} from './expense.service'
+import {GetUserId} from '../auth/decorators'
 
 @Controller('expense')
-export class ExpenseController {}
+export class ExpenseController {
+  constructor(private expenseService: ExpenseService) {}
+
+  @Get()
+  getExpenses(@GetUserId() id: number) {
+    return this.expenseService.getExpenses(id)
+  }
+
+  @Get(':id')
+  getExpense(@GetUserId() id: number, @Param('id') expenseId: number) {
+    return this.expenseService.getExpense(id, expenseId)
+  }
+
+  @Post()
+  createExpense(@GetUserId() id: number) {}
+
+  @Patch(':id')
+  updateExpense(@GetUserId() id: number) {}
+
+  @Delete(':id')
+  deleteExpense(@GetUserId() id: number) {}
+}
