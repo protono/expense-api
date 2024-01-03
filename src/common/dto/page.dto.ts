feef55533ef1f63110de81d77314974cd1977079
Sylvain Pelissier <sylvain@onenetbeyond.org>
2024-01-03 14:31:30 +0100

    cap pagination params

--- src/common/dto/page.dto.ts
+++ src/common/dto/page.dto.ts
@@ -1,14 +1,16 @@
 import {Type} from 'class-transformer'
-import {IsNumber, IsOptional} from 'class-validator'
+import {IsNumber, IsOptional, Max} from 'class-validator'
 
 export class PageDTO {
   @IsNumber()
   @IsOptional()
   @Type(() => Number)
+  @Max(25)
   take: number = 10
 
   @IsNumber()
   @IsOptional()
   @Type(() => Number)
+  @Max(5)
   skip: number = 0
 }
