48e6d436dd712dd3b8d7a42534f31a7177d4298f
Sylvain Pelissier <sylvain@onenetbeyond.org>
2024-01-08 16:49:38 +0100

    the logger

--- src/scheduler/scheduler.service.ts
+++ src/scheduler/scheduler.service.ts
@@ -1,10 +1,42 @@
-import {Injectable} from '@nestjs/common'
+import {Injectable, Logger} from '@nestjs/common'
 import {Cron, CronExpression, Interval, Timeout} from '@nestjs/schedule'
+import {PrismaService} from '../prisma/prisma.service'
+import {Expense} from '@prisma/client'
 
 @Injectable()
 export class SchedulerService {
-  @Cron(CronExpression.EVERY_5_SECONDS)
-  scheduledTask() {
-    console.log('Running scheduled task')
+  constructor(private prismaService: PrismaService) {}
+
+  private logger = new Logger(SchedulerService.name)
+
+  @Cron('*/30 * * * * *') // EVERY_30_SECONDS
+  async calculateCurrentBalance() {
+    const users = await this.prismaService.user.findMany({
+      include: {
+        expenses: true,
+      },
+    })
+    for (const user of users) {
+      const totalExpenses = user.expenses.reduce((prev: number, next: Expense) => {
+        return prev + +next.amount
+      }, 0)
+
+      if (user.initialBalance - totalExpenses >= user.currentBalance) continue
+
+      await this.prismaService.user
+        .update({
+          where: {
+            id: user.id,
+          },
+          data: {
+            currentBalance: user.initialBalance - totalExpenses,
+          },
+        })
+        .catch((error) => {
+          this.logger.error(error)
+        })
+    }
+
+    this.logger.log(`calculateCurrentBalance() ran for ${users.length} users`)
   }
 }
