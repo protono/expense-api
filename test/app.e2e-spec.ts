c15b0ae1be1c9bd6872e73f69397160bddb970e2
Sylvain Pelissier <sylvain@onenetbeyond.org>
2024-01-12 14:42:04 +0100

    configure e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,65 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import {Test} from '@nestjs/testing'
+import {AuthModule} from '../src/auth/auth.module'
+import {PrismaModule} from '../src/prisma/prisma.module'
+import {UserModule} from '../src/user/user.module'
+import {ExpenseModule} from '../src/expense/expense.module'
+import {SchedulerModuile} from '../src/scheduler/scheduler.module'
+import {ScheduleModule} from '@nestjs/schedule'
+import {ConfigModule, ConfigService} from '@nestjs/config'
+import {CacheModule} from '@nestjs/cache-manager'
+import {RedisClientOptions} from 'redis'
+import {redisStore} from 'cache-manager-redis-yet'
+import {APP_GUARD} from '@nestjs/core'
+import {AdminGuard, SessionGuard} from '../src/auth/guards'
+import {INestApplication} from '@nestjs/common'
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+describe('App (e2e)', () => {
+  let app: INestApplication
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      imports: [
+        AuthModule,
+        CacheModule.registerAsync<RedisClientOptions>({
+          isGlobal: true,
+          inject: [ConfigService],
+          useFactory: (configService: ConfigService) => {
+            return {
+              ttl: 10000, // milliseconds
+              url: configService.getOrThrow('REDIS_URL'),
+              store: redisStore,
+            }
+          },
+        }),
+        ConfigModule.forRoot({isGlobal: true}),
+        ExpenseModule,
+        PrismaModule,
+        ScheduleModule.forRoot(),
+        SchedulerModuile,
+        UserModule,
+      ],
+      providers: [
+        {
+          provide: APP_GUARD,
+          useClass: SessionGuard,
+        },
+        {
+          provide: APP_GUARD,
+          useClass: AdminGuard,
+        },
+      ],
+    }).compile()
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+    app = module.createNestApplication()
+  })
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
-});
+  afterAll(() => {
+    app.close()
+  })
+
+  describe('[AppModule]', () => {
+    it('should be defined', () => {
+      expect(app).toBeDefined()
+    })
+  })
+})
